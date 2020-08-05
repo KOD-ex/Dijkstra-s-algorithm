382b9ff28f0ba428e66d57c2965e227fefa6131f
KOD-ex <64311703+KOD-ex@users.noreply.github.com>
2020-08-05 13:34:47 +0000

    Update Dijkstras_algorithm.h

--- Dijkstras_algorithm.h
+++ Dijkstras_algorithm.h
@@ -3,6 +3,3 @@ int Dijkstras_algorithm(int graph[][4], int costs[], int parents[]);
 
 //найти вершину с наименьшей стоимостью среди необработанных
 int finding_the_cheapest_vertex(int costs[]);
-
-//распечатка графа
-void print_graph(int graph[][4]);
